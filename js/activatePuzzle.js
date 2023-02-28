c58e306c24f402b6ecbd82b07abf778bc08fc188
Simenya Jonathan Enoch <62070508+Simenya@users.noreply.github.com>
2023-02-28 09:50:38 +0300

    Add files via upload

---
+++ js/activatePuzzle.js
@@ -0,0 +1,65 @@
+
+
+var options = {
+    solution: 			[[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                            [null,	null,	null,	null,	null,	null,	null,	null,	null,    "C",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                            [null,	null,	null,	null,	null,	 "W",	null,	null,	null,    "L",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	 "M",	 "E",	 "T",	 "H",	 "A",	 "N",	 "E",	null,    "E",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	 "S",	null,	null,	null,    "A",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	 "H",	"U",	 "M",	"A",     "N",   "S",   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,    "E",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	 "P",	 "O",	 "L",	 "L",	 "U",	"T",	 "I",	 "O",    "N",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,    "E",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	 "A",	 "F",	 "F",	 "O",    "R",    "E",    "S",    "T",    "A",    "T",    "I",     "O",    "N",   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,    "G",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,    "Y",   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+
+     index: 				[[null,	null,	 null,	 null,	 null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	 null,	 null,	 null,	null,	 null,	 null,	 null,   3,      null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,  null,    null,    null,   null,   2,      null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,      1,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   5,      null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   4,      null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   6,      null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+  questions_across: [	  
+                           "1. The main greenhouse gas emitted by agriculture",
+                           "4. Introduction of harmful substances or pollutants into the natural environment, causing adverse effects on the health and well-being of living organisms",
+                           "5. Most responsible for climatic change",
+                           "6. Key strategy in mitigating climate change by absorbing carbon dioxide from the atmosphere through photosynthesis"
+                     ],
+
+  questions_down: [		 
+                          "2.  Acronym  for interrelated areas that are essential for public health and well-being, especially in developing countries",
+                          "3. Energy sources and technologies that produce little to no greenhouse gas emissions",
+                 ]
+
+};
+
+
+cwp_nmsp.activateCWP(document.getElementById("myPuzzle"),false,options);
